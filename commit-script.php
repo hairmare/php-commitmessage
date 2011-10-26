c5466cd90e4b90ff41bcacc3d788f7846798b4c4
Lucas Bickel <hairmare@purplehaze.ch>
2011-10-26 21:58:13 +0200

    same handling for IssueCheck

--- commit-script.php
+++ commit-script.php
@@ -11,33 +11,7 @@ require_once 'src/CommitMessage/Analyse.php';
 require_once 'src/CommitMessage/HandlerFactory.php';
 require_once 'src/CommitMessage/HandlerStack.php';
 require_once 'src/CommitMessage/Handler/Issue.php';
-/**
- * look if issue is in db and commiter has rights on issue
- */
-class CommitMessage_Handler_IssueCheck extends CommitMessage_Handler_Issue
-{
-    protected $_statusMap = array(
-        1 => 2
-    );
-    public function run() {
-        // get issue
-        $this->_initRedmine();
-        $this->_redmine->find($this->getIssueId());
-
-        // check status
-        $statusId = (int) $this->_redmine->status['id'];
-
-        // change status if needed
-        $statusMap = $this->_statusMap;
-        $statusKeys = array_keys($statusMap);
-        if (in_array($statusId, $statusKeys)) {
-            $setdev = new CommitMessage_Handler_IssueChangeStatus;
-            $setdev->setIssueId($this->getIssueId());
-            $setdev->setNewStatus($statusMap[$statusId]);
-            $this->getCaller()->append($setdev);
-        }
-    }
-}
+require_once 'src/CommitMessage/Handler/IssueCheck.php';
 /**
  * add a comment to the issue and create a link to websvn in the comment
  */
---
+++ src/CommitMessage/Handler/IssueCheck.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'src/CommitMessage/Handler/Issue.php';
+
+/**
+ * look if issue is in db
+ */
+class CommitMessage_Handler_IssueCheck extends CommitMessage_Handler_Issue
+{
+    protected $_statusMap = array(
+        1 => 2
+    );
+    public function run() {
+        // get issue
+        $this->_initRedmine();
+        $this->_redmine->find($this->getIssueId());
+
+        // check status
+        $statusId = (int) $this->_redmine->status['id'];
+
+        // change status if needed
+        $statusMap = $this->_statusMap;
+        $statusKeys = array_keys($statusMap);
+        if (in_array($statusId, $statusKeys)) {
+            $setdev = new CommitMessage_Handler_IssueChangeStatus;
+            $setdev->setIssueId($this->getIssueId());
+            $setdev->setNewStatus($statusMap[$statusId]);
+            $this->getCaller()->append($setdev);
+        }
+    }
+}
+
