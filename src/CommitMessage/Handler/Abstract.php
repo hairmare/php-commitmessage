40655d966afaf155d7648acd501c98784007ce77
Lucas Bickel <hairmare@purplehaze.ch>
2011-10-26 21:08:29 +0200

    checkstyle

--- src/CommitMessage/Handler/Abstract.php
+++ src/CommitMessage/Handler/Abstract.php
@@ -5,7 +5,8 @@ require_once 'src/CommitMessage/Handler/Interface.php';
 /**
  *
  */
-abstract class CommitMessage_Handler_Abstract implements CommitMessage_Handler_Interface
+abstract class CommitMessage_Handler_Abstract
+    implements CommitMessage_Handler_Interface
 {
     private $_caller = false;
     public function setCaller($caller)
