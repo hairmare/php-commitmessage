96754145fe785bcd12ed4497a7a0a3ce9dfd26fc
Lucas Bickel <hairmare@purplehaze.ch>
2011-10-27 11:48:37 +0200

    start refactoring last commitmessage class

--- commit-script.php
+++ commit-script.php
@@ -13,29 +13,8 @@ require_once 'src/CommitMessage/HandlerStack.php';
 require_once 'src/CommitMessage/Handler/Issue.php';
 require_once 'src/CommitMessage/Handler/IssueCheck.php';
 require_once 'src/CommitMessage/Handler/IssueDecorate.php';
+require_once 'src/CommitMessage/Handler/IssueChangeStatus.php';
 
-class CommitMessage_Handler_IssueChangeStatus extends CommitMessage_Handler_Issue
-{
-    private $_newStatus = false;
-
-    public function setNewStatus($newStatus)
-    {
-        $this->_newStatus = $newStatus;
-    }
-
-    public function run()
-    {
-        if (!$this->_newStatus) {
-            throw new Exception('IssueChangeStatus called without newStatus');
-        }
-
-        $this->_initRedmine();
-        $this->_redmine->find($this->getIssueId());
-        $this->_redmine
-             ->set('status_id', $this->_newStatus)
-             ->save();
-    }
-}
 
 require_once('lib/phpactiveresource-0.14-beta/ActiveResource.php');
 
---
+++ src/CommitMessage/Handler/IssueChangeStatus.php
@@ -0,0 +1,27 @@
+<?php
+
+class CommitMessage_Handler_IssueChangeStatus
+    extends CommitMessage_Handler_Issue
+{
+    private $_newStatus = false;
+
+    public function setNewStatus($newStatus)
+    {
+        $this->_newStatus = $newStatus;
+    }
+
+    public function run()
+    {
+        if (!$this->_newStatus) {
+            throw new Exception('IssueChangeStatus called without newStatus');
+        }
+
+        $this->_initRedmine();
+        $this->_redmine->find($this->getIssueId());
+        $this->_redmine
+             ->set('status_id', $this->_newStatus)
+             ->save();
+    }
+}
+
+
