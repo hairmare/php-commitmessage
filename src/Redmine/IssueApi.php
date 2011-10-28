23c171442d70271830dfe9e119442febba3421d5
Lucas Bickel <hairmare@purplehaze.ch>
2011-10-28 09:26:07 +0200

    new style api is cleaner and more open
    btw this class is more a local high level api for internal use than
    really something you should be using to integrate elsewhere. I am
    mainly useing it so i can isolate my logic from the core components
    of the service api in a somewhat sane manner while still keeping the
    code testable in ci.

--- src/Redmine/IssueApi.php
+++ src/Redmine/IssueApi.php
@@ -10,36 +10,45 @@ class Redmine_IssueApi
 {
     private $_issue = false;
 
-    public function find($search, $options = array())
+    public function find($searchExpr, $options = array())
     {
         $this->_lazyInit();
-        $this->_issue->find($search, $options);
+        $this->_issue->find($searchExpr, $options);
     }
     public function getStatusId()
     {
         return (int) $this->_issue->status['id'];
     }
-    public function setStatusId($status, $trackerId = NULL)
+    public function setStatusId($trackerId, $newStatusId)
     {
         $this->_lazyInit();
-        if (!$trackerId) {
-            $this->_issue->find($trackerId);
-        }
-        return $this->_issue->set('status_id', $status)->save();
+        $this->_issue->find($trackerId);
+        return $this->_issue->set('status_id', $newStatusId)->save();
     }
-    public function addNoteToTicket($note, $trackerId = NULL)
+    public function addNoteToTicket($trackerId, $newNoteText)
     {
         $this->_lazyInit();
-        if (!$trackerId) {
-            $this->_issue->find($trackerId);
-        }
-        $this->_issue->set('notes', $note)->save();
+        $this->_issue->find($trackerId);
+        $this->_issue->set('notes', $newNoteText)->save();
     }
-    private function _lazyInit()
+    protected function _setIssue($issue)
+    {
+        $this->_issue = $issue;
+    }
+    protected function _lazyInit()
     {
         if (!$this->_issue) {
-            $this->_issue = new Issue;
+            $this->_setIssue(new Issue);
+            $this->_onInit();
         }
+    }
+    /**
+     * helper for proper unit testing and future expansion
+     */
+    private function _onInit()
+    {
+        // i can add tests to make sure this is called if you plan on using it
+        return;
     }
 
 }
