a1d18b09651373e87c07f5339ba5f4ed27d0836d
Lucas Bickel <hairmare@purplehaze.ch>
2012-01-26 23:18:32 +0100

    something to release?
    my hacking somehow brought forth a rpm that isn't humonguous, at
    least not as bad as i originally feared. This push is mainly the
    results of quite some work on the whole stack. Since I'm layz I
    plan on starting with some serious dox on the next push ;)

--- src/Redmine/ActiveResource.php
+++ src/Redmine/ActiveResource.php
@@ -6,6 +6,10 @@ class Redmine_ActiveResource extends ActiveResource
     {
         $this->request_format = 'xml';
     }
+    public function setSite($site)
+    {
+        $this->site = $site;
+    }
     var $site = 'http://b3126476278c49215556ed00592bd331bb8e65d3:@192.168.1.109:3000/';
 }
 
