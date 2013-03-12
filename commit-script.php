80f5fdd08fbce0c7804f492c13809e2e07813aed
Lucas Bickel <hairmare@purplehaze.ch>
2013-03-12 19:27:43 +0100

    remove require for move to bootstrap

--- commit-script.php
+++ commit-script.php
@@ -5,23 +5,7 @@
  * This does most of the heavy lifting for tightly integrating
  * the redmine instance with the other tools needed for the job.
  */
-
-/* active resource gets include here so i can haz clean
- * coverge in builds. This should be in ActiveResource.php
- * and it will get removed when i do autoloading anyhow.
- */
-require_once 'lib/phpactiveresource/ActiveResource.php';
-
-/* these are my own working, they to need autoloading
- */
-require_once 'src/CommitMessage/Split.php';
-require_once 'src/CommitMessage/Analyse.php';
-require_once 'src/CommitMessage/Factory.php';
-require_once 'src/CommitMessage/HandlerStack.php';
-require_once 'src/CommitMessage/Handler/Issue.php';
-require_once 'src/CommitMessage/Handler/IssueCheck.php';
-require_once 'src/CommitMessage/Handler/IssueDecorate.php';
-require_once 'src/CommitMessage/Handler/IssueChangeStatus.php';
+require_once 'src/Bootstrap.php';
 
 // testing...
 $test = 'ich commit mal was...
